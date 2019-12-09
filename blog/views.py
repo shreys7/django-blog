c98d938e4c87274cb5be78f6b15c9e780804f32b
shreys7 <shreyshah97@gmail.com>
2019-12-10 00:21:55 +0530

    Added generic and detail views, and added urls.py mapppings

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class Postdetail(generic.DetailView):
+    model = Post
+    template_name = 'detail.html'
