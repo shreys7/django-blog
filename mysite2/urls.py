d33716f3ac03518c69a0e83f074d7de996875f91
shreys7 <shreyshah97@gmail.com>
2019-12-12 02:18:52 +0530

    Include signup and signup_submit urls

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -6,6 +6,8 @@ from . import auth_views
 urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('authenticate/', auth_views.authenticate, name='authenticate'),
+    path('signup', auth_views.signup, name='signup'),
+    path('signup/submit', auth_views.signup_submit, name='signup_submit'),
     path('', views.index),
     path('admin/', admin.site.urls),
     path('blogs/', include('blog.urls'))
