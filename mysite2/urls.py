538c889d702b099c5a0cf20610c383c789b4a4d7
shreys7 <shreyshah97@gmail.com>
2019-12-11 23:26:07 +0530

    Added redirection of /blogs page when landing on empty path(Homepage)

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('blogs/', include('blog.urls'))
 ]
