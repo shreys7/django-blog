4a33031497bfb4b12c6af27cd1f76e92efb61309
shreys7 <shreyshah97@gmail.com>
2019-12-14 02:40:27 +0530

    Add logout url pattern in urls.py

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -5,6 +5,7 @@ from . import auth_views
 
 urlpatterns = [
     path('login/', auth_views.login, name='login'),
+    path('logout', auth_views.logout, name='logout'),
     path('authenticate/', auth_views.authenticate, name='authenticate'),
     path('signup', auth_views.signup, name='signup'),
     path('signup/submit', auth_views.signup_submit, name='signup_submit'),
